c84a1a73e1916a633883d4dc1d68b6b64b50b244
towoeshi <yatroman2018@gmail.com>
2026-05-11 01:29:56 +0400

    change program exit success

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ def main():
             print(repr(collection))
 
         elif inp == "0":
-            print("Программа завершена.")
+            print("Программа завершена успешно.")
             break
 
         else:
